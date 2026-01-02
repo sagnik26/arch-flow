9a7a8ad7e3bf3ca4ba33e46658b0aef4d83cbbb2
Cbz <cbz704150@gmail.com>
2026-01-03 01:50:01 +0530

    Project initialized

---
+++ src/main/java/com/archflow/archigen/ArchigenApplication.java
@@ -0,0 +1,13 @@
+package com.archflow.archigen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchigenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchigenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/archflow/archigen/ArchigenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archflow.archigen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchigenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
